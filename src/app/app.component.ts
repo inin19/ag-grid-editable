bbcd7922a4fd21fceaec574996b15bd722806823
Chen, Ying Ying <Ying-Ying.Chen@marsh.com>
2019-08-12 22:36:32 -0400

    create

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { NumberFormatterComponent } from './custom-components/number-formatter.component';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ag-app-editable';
+
+  columnDefs = [
+    { headerName: 'Make', field: 'make' },
+    { headerName: 'Model', field: 'model' },
+    {
+      headerName: 'Price', field: 'price', editable: true,
+      /* specify custom cell renderer */
+      cellRenderer: 'numberFormatterComponent',
+
+    }
+  ];
+
+  frameworkComponents = {
+    numberFormatterComponent: NumberFormatterComponent,
+  };
+
+  rowData: any;
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit() {
+    this.rowData = this.http.get('https://api.myjson.com/bins/15psn9');
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppComponent } from './app.component';
 import { AgGridModule } from 'ag-grid-angular';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NumberFormatterComponent } from './custom-components/number-formatter.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NumberFormatterComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AgGridModule.withComponents([]),
     BrowserAnimationsModule
 
